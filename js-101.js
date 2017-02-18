fc611eb9f6b229712dc06fe3feaae89c2dabc9cd
drworm75 <dwgames@gmail.com>
2017-02-18 12:44:42 -0600

    fixed dash in song additon scripts

--- js-101.js
+++ js-101.js
@@ -1,5 +1,7 @@
 var songs = [];
-var correctedSongs = [];
+var songString = "";
+var element = document.getElementById("song-info");
+var songContainer = element.innerHTML;
 
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
@@ -7,11 +9,27 @@ songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album Th
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
+function addSongEnd(title, artist, album) {
+	console.log(title);
+	songs.push(title + "- by " + artist + " on the album " + album);
+}
+
+function addSongBegining(title, artist, album) {
+	console.log(title);
+	songs.unshift(title + "- by " + artist + " on the album " + album);
+}
 
 for (var i = 0; i < songs.length; i++) {
 	songs[i] = (songs[i].replace(/[^0-9a-z>" "]/gi, ''));
 	songs[i] = (songs[i].replace(/>/gi, '-'));
 }
 
+addSongBegining("Wait for it", "Leslie Odam, Jr.", "Hamilton");
+addSongEnd("Boombastic", "Shaggy", "Boombastic");
 
-console.log(songs);
+for (var i = 0; i < songs.length; i++) {
+	songString += songs[i] + "<br>";
+}
+
+element.innerHTML = songString;
+console.log(songContainer);
