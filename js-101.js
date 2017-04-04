e76a269e70008cb85f3359169acbe17e0fbb447b
drworm75 <dwgames@gmail.com>
2017-04-03 20:10:47 -0500

    Functions correctly

--- js-101.js
+++ js-101.js
@@ -1,50 +1,29 @@
-// var songs = [];
-// var songString = "";
 var element = document.getElementById("song-info");
-// var songContainer = element.innerHTML;
+var moreBtn = document.getElementById("button");
 
-// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-
-// function addSongEnd(title, artist, album) {
-// 	songs.push(title + "- by " + artist + " on the album " + album);
-// }
-
-// function addSongBegining(title, artist, album) {
-// 	console.log(title);
-// 	songs.unshift(title + "- by " + artist + " on the album " + album);
-// }
-
-// for (var i = 0; i < songs.length; i++) {
-// 	songs[i] = (songs[i].replace(/[^0-9a-z&>" "]/gi, ''));
-// 	songs[i] = (songs[i].replace(/>/gi, '-'));
-// }
-
-// addSongBegining("Wait for it", "Leslie Odam, Jr.", "Hamilton");
-// addSongEnd("Boombastic", "Shaggy", "Boombastic");
-
-// for (var i = 0; i < songs.length; i++) {
-// 	songString += songs[i] + "<br>";
-// }
-
-
-
-
-function buildDom() {
-	var musicData = JSON.parse(this.responseText);
+function buildDom(music) {
 	var musicChart = ""
+	for (key in music) {
+		if (key === "default-music") {
+			var musicData = music["default-music"]		
+		}
+		else if (key === "user-music") {
+			var musicData = music["user-music"]
+		}
+	}
+	if (musicData === music["user-music"]) {
+		musicChart = element.innerHTML;
+	}
+
 	musicChart += `<table>`
-	for (var i = 0; i < musicData["default-music"].length; i++) {
+	for (var i = 0; i < musicData.length; i++) {
 		musicChart += `<tr>`
-		musicChart += `<th colspan="1"><a class="delete-button" href="#">X</a>&nbsp;&nbsp;&nbsp;${musicData["default-music"][i].title}</th>`
+		musicChart += `<th colspan="3"><a class="delete-button" href="#">X</a>&nbsp;&nbsp;&nbsp;${musicData[i].title}</th>`
 		musicChart += `</tr>`
 		musicChart += `<tr>`
-		musicChart += `<td>${musicData["default-music"][i].artist}</td>`	
-		musicChart += `<td>${musicData["default-music"][i].album}</td>`	
-		musicChart += `<td>${musicData["default-music"][i].genre}</td>`	
+		musicChart += `<td>${musicData[i].artist}</td>`	
+		musicChart += `<td>${musicData[i].album}</td>`	
+		musicChart += `<td>${musicData[i].genre}</td>`	
 		musicChart += `</tr>`
 	}
 		musicChart += `</table>`
@@ -52,12 +31,17 @@ function buildDom() {
 		element.innerHTML = musicChart;
 }
 
+function executeRequest() {
+	var data = JSON.parse(this.responseText);
+	buildDom(data);
+}
+
 function executeThisCodeAfterFileFails() {
 	alert("File not found!");
 }
 
 var myRequest = new XMLHttpRequest(); 
-myRequest.addEventListener("load", buildDom); //Look at createCategoriesArray
+myRequest.addEventListener("load", executeRequest); //Look at createCategoriesArray
 myRequest.addEventListener("error", executeThisCodeAfterFileFails);
 myRequest.open("GET", "default-music.JSON");
 myRequest.send();
@@ -68,3 +52,15 @@ document.body.addEventListener("click", function(event) {
 		event.target.parentNode.parentNode.remove();
 	}
 });
+
+document.body.addEventListener("click", function(event) {
+	if (event.target.className === "button") {
+		event.target.remove();
+		var myRequest2 = new XMLHttpRequest(); 
+		myRequest2.addEventListener("load", executeRequest); //Look at createCategoriesArray
+		myRequest2.addEventListener("error", executeThisCodeAfterFileFails);
+		myRequest2.open("GET", "user-music.JSON");
+		myRequest2.send();
+	}
+});
+
