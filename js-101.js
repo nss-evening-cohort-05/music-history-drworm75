e426c6c954d7c597ef121e9f24c575949eb008f9
drworm75 <dwgames@gmail.com>
2017-04-08 09:18:59 -0500

    Replaced vjs with jQuery when possible.

--- js-101.js
+++ js-101.js
@@ -1,66 +1,47 @@
-var element = document.getElementById("song-info");
-var moreBtn = document.getElementById("button");
-
-function buildDom(music) {
-	var musicChart = ""
-	for (key in music) {
+function buildDom(music) { 
+	var musicChart = "";
+	var musicData;
+	$.each(music, function(key) {
 		if (key === "default-music") {
-			var musicData = music["default-music"]		
+			musicData = music["default-music"];
+			$("#song-info").html("");
+			$("#song-info").append(`<table>`);
 		}
-		else if (key === "user-music") {
-			var musicData = music["user-music"]
+		else {
+			musicData = music["user-music"];
 		}
-	}
-	if (musicData === music["user-music"]) {
-		musicChart = element.innerHTML;
-	}
+	});
 
-	musicChart += `<table>`
-	for (var i = 0; i < musicData.length; i++) {
-		musicChart += `<tr>`
-		musicChart += `<th colspan="3"><a class="delete-button" href="#">X</a>&nbsp;&nbsp;&nbsp;${musicData[i].title}</th>`
-		musicChart += `</tr>`
-		musicChart += `<tr>`
-		musicChart += `<td>${musicData[i].artist}</td>`	
-		musicChart += `<td>${musicData[i].album}</td>`	
-		musicChart += `<td>${musicData[i].genre}</td>`	
-		musicChart += `</tr>`
-	}
-		musicChart += `</table>`
-		musicChart += `<a class="button" href="#">More  ></a>`
-		element.innerHTML = musicChart;
+	$.each(musicData, function(index, key) {
+		$("#song-info").append(`<tr><th colspan="3"><a class="delete-button" href="#">X</a>&nbsp;&nbsp;&nbsp${key.title}</th></tr>`);
+		$("#song-info").append(`<tr><td>${key.artist}</td><<td>${key.album}</td><<td>${key.genre}</td></tr>`);
+	});
+	$("#song-info").append(`<a class="button" href="#">More  ></a>`);
 }
 
-function executeRequest() {
-	var data = JSON.parse(this.responseText);
+$.ajax({
+  url: "default-music.JSON"
+}).done(function(data){
 	buildDom(data);
-}
-
-function executeThisCodeAfterFileFails() {
-	alert("File not found!");
-}
-
-var myRequest = new XMLHttpRequest(); 
-myRequest.addEventListener("load", executeRequest); //Look at createCategoriesArray
-myRequest.addEventListener("error", executeThisCodeAfterFileFails);
-myRequest.open("GET", "default-music.JSON");
-myRequest.send();
-
-document.body.addEventListener("click", function(event) {
-	if (event.target.className === "delete-button") {
-		event.target.parentNode.parentNode.nextSibling.remove();
-		event.target.parentNode.parentNode.remove();
-	}
+}).fail(function(error){
+  console.log(error);
 });
 
-document.body.addEventListener("click", function(event) {
-	if (event.target.className === "button") {
-		event.target.remove();
-		var myRequest2 = new XMLHttpRequest(); 
-		myRequest2.addEventListener("load", executeRequest); //Look at createCategoriesArray
-		myRequest2.addEventListener("error", executeThisCodeAfterFileFails);
-		myRequest2.open("GET", "user-music.JSON");
-		myRequest2.send();
-	}
+$(document).ready(function() {
+	$(".button").on('click', function(event) {
+		$(this).remove();
+		$.ajax({
+		  url: "user-music.JSON"
+		}).done(function(data){
+			buildDom(data);
+		}).fail(function(error){
+		  console.log(error);
+		});		
+	});
+	$(document).on('click', '.delete-button', function(event) {
+		$(this).closest("tr").next("tr").remove();
+		$(this).closest("tr").remove();
+	});
 });
+
 
